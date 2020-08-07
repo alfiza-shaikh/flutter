5c45db4fb5df71e4a9f0333fb045412e63ad53b1
alfiza-shaikh <50302756+alfiza-shaikh@users.noreply.github.com>
2020-08-07 11:21:50 +0530

    lecture basic 1

--- Dart/Lecture1.dart
+++ Dart/Lecture1.dart
@@ -1,1 +1,12 @@
 //lecture 1
+import 'dart:io';
+
+void main() {
+  print('welcome');
+  //stdout.writeln('What \'s your name ? ');
+  print('What \'s your name ? ');
+  String name = stdin.readLineSync();
+  print('So,Your name is $name');
+  var name2 = 'alfiza';
+  print('My age is $name2');
+}
